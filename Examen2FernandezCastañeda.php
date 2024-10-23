cf5f5eaa63e78655469c73b4078aa531a27e72aa
DanielFernandez05 <christian.fernandez.castaneda@cbtis89.edu.mx>
2024-10-23 09:44:54 -0600

    Add files via upload

---
+++ Examen2FernandezCastañeda.php
@@ -0,0 +1,25 @@
+<?php
+/* CBTIS 89
+Programa que almacena los precios de articulos en un array y calcula el IVA y el descuento
+Christian Daniel Fernandez Castañeda
+3°A Programación Matutino
+*/
+echo "*ARREGLO PRECIOS*";
+echo "<br><br>";
+$arrayPrecios = array(200,300,500,400);
+
+echo "*ARREGLO IVA*";
+$arrayIVA = array(0.16);
+
+for($i=0; $i<$arrayPrecios; $i++)
+
+$IVA = $arrayPrecios*$arrayIVA;
+
+$longitud = count($arrayPrecios);
+for($i=0; $i<$longitud; $i++)
+{$arrayIVA[$i] = $arrayPrecios[$i]*$arrayIVA[$i];
+
+}
+	
+
+	?>
